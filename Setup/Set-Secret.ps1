de59de6d211fd42378e2a75dc5ef70266944b714
Brijesh Patel <brijeshpatel@gmail.com>
2024-11-15 12:05:19 +1100

    EET-506: Adding Set-Secret setup script
    Execute the script by running the following PowerShell command:
    .\Set-Secret.ps1 -SecretKey "MyGraphApiSecretKey" -SecretValue "<SECRET GOES HERE>"

---
+++ Setup/Set-Secret.ps1
@@ -0,0 +1,16 @@
+﻿param (
+    [string]$SecretKey,
+    [string]$SecretValue
+)
+
+# Check if the environment variable already exists
+if ([System.Environment]::GetEnvironmentVariable($SecretKey, [System.EnvironmentVariableTarget]::User)) {
+    Write-Host "Updating existing environment variable..."
+} else {
+    Write-Host "Creating new environment variable..."
+}
+
+# Set the environment variable
+[System.Environment]::SetEnvironmentVariable($SecretKey, $SecretValue, [System.EnvironmentVariableTarget]::User)
+
+Write-Host "Environment variable '$SecretKey' has been set to '$SecretValue'."
